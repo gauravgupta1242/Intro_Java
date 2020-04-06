1e57afd45ce771a2ac70298478e1d507f454a481
Gaurav Gupta <gauravgupta1242@gmail.com>
2020-04-06 20:27:38 +0530

    Basic Java Program

---
+++ src/PerefectNumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter the number ");
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+				
+		
+		boolean b = isPerfect(n);
+		if(b)
+			System.out.printf("%d is Perfect Number",n);
+		else
+			System.out.printf("%d is not Perfect Number",n);
+
+	}
+	
+	// Define the method isPerfect()
+	
+	public static boolean isPerfect(int n ) {
+		
+		int sum = 0 ;
+		
+		for (int i = 1 ; i<= n/2 ; i++)
+		{
+			if(n%i == 0)
+				sum += i;
+		}
+		
+		if(n==sum)
+			return true;
+		
+		return false;
+		
+	}
+	
+}
